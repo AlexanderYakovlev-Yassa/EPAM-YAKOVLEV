5bd0dc7336d72a677a69941ad0cd30754c278c4f
Alexander Yakovlev <sashayassa@gmail.com>
2020-01-31 10:59:57 +0300

    Change SQL scripts

--- SQL_scripts/queries.sql
+++ SQL_scripts/queries.sql
@@ -6,6 +6,20 @@ SELECT daytime, value FROM finance_operation;
 
 SELECT daytime, value FROM finance_operation WHERE type_id = 3;
 
+UPDATE operation_type
+    SET type='internet'
+    WHERE type='wi-fi';
+    
+UPDATE finance_operation
+    SET value=80.50
+    WHERE value=80.00;
+    
+DELETE FROM finance_operation
+    WHERE type_id =3;    
+
 SELECT t1.daytime, t2.type, t1.value FROM finance_operation AS t1, operation_type AS t2
   WHERE t1.type_id = t2.id
   ORDER BY t1.daytime;
+  
+
+    
